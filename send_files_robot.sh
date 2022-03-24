9f9b31d3198dda2875bf1e8967aa9592960944d9
unknown <igor.lirussi@studio.unibo.it>
2022-03-24 13:21:40 +0300

    improved scripts

--- send_files_robot.sh
+++ send_files_robot.sh
@@ -1,4 +1,4 @@
 echo "Insert psw to acces the robot account"
-scp *.py ruser@79.123.176.35:~/igor/Baxter-Robot-ObjDet
+scp *.py ruser@$(cat ip_robot.txt):~/igor/Baxter-Robot-ObjDet
 echo "#### Done, press any key ####"
 read -t 5 -n 1
---
+++ ssh_connect_robot.sh
@@ -0,0 +1,2 @@
+echo "Insert psw to acces the robot account"
+ssh ruser@$(cat ip_robot.txt)
